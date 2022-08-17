5208b9b634d9040801876d5d6cb688e7884da0bc
anusha <anushaaryool@gmail.com>
2022-08-17 10:50:39 -0500

    Feature Mortgage Calculator

--- src/com/learning/java/Main.java
+++ src/com/learning/java/Main.java
@@ -1,8 +1,33 @@
 package com.learning.java;
 
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	final byte PERCENT = 100;
+    final byte MONTHS_IN_YEAR = 12;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
+
+        System.out.print("Interest: ");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+
+        System.out.print("Period(Years): ");
+        int period = scanner.nextInt();
+        int totalMonths = period * MONTHS_IN_YEAR;
+
+        double mortgage = principal
+                * ( monthlyInterest * Math.pow( 1+ monthlyInterest, totalMonths))
+                / Math.pow(1 + monthlyInterest, totalMonths) - 1;
+
+        String formattedMortgage = NumberFormat.getCurrencyInstance().format(mortgage);
+
+        System.out.println("Mortgage: " + formattedMortgage);
     }
 }
